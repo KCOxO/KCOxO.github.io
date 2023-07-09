add76a9ec6a7f8a9bac4ce753fa7f1a5a41ebae2
KC <daisy080512@gmail.com>
2023-07-09 17:51:53 +0800

    2023-07-09

--- Javascript/2023-06-26-HW/ipad_Air.js
+++ Javascript/2023-06-26-HW/ipad_Air.js
@@ -1,47 +1,167 @@
 [
     {
-        "color":"gray",
-        "data":[
-            {"color": "太空灰色", "price": "19900", "storage": "64GB", "capacity": "Wi-Fi", "image": "ipadAir_gray1" },
-            {"color": "太空灰色", "price": "24900", "storage": "64GB", "capacity": "Wi-Fi+行動網路", "image": "ipadAir_gray1" },
-            {"color": "太空灰色", "price": "24900", "storage": "256GB", "capacity": "Wi-Fi", "image": "ipadAir_gray1" },
-            {"color": "太空灰色", "price": "29900", "storage": "256GB", "capacity": "Wi-Fi+行動網路", "image": "ipadAir_gray1" }
+        "color": "gray",
+        "data": [
+            {
+                "color": "太空灰色",
+                "price": "19900",
+                "storage": "64GB",
+                "capacity": "Wi-Fi",
+                "image": "ipadAir_gray1"
+            },
+            {
+                "color": "太空灰色",
+                "price": "24900",
+                "storage": "64GB",
+                "capacity": "Wi-Fi+行動網路",
+                "image": "ipadAir_gray1"
+            },
+            {
+                "color": "太空灰色",
+                "price": "24900",
+                "storage": "256GB",
+                "capacity": "Wi-Fi",
+                "image": "ipadAir_gray1"
+            },
+            {
+                "color": "太空灰色",
+                "price": "29900",
+                "storage": "256GB",
+                "capacity": "Wi-Fi+行動網路",
+                "image": "ipadAir_gray1"
+            }
         ]
     },
     {
-        "color":"blue",
-        "data":[
-            {"color": "藍色", "price": "19900", "storage": "64GB", "capacity": "Wi-Fi", "image": "ipadAir_blue1" },
-            {"color": "藍色", "price": "24900", "storage": "64GB", "capacity": "Wi-Fi+行動網路", "image": "ipadAir_blue1" },
-            {"color": "藍色", "price": "24900", "storage": "256GB", "capacity": "Wi-Fi", "image": "ipadAir_blue1" },
-            {"color": "藍色", "price": "29900", "storage": "256GB", "capacity": "Wi-Fi+行動網路", "image": "ipadAir_blue1" },
+        "color": "blue",
+        "data": [
+            {
+                "color": "藍色",
+                "price": "19900",
+                "storage": "64GB",
+                "capacity": "Wi-Fi",
+                "image": "ipadAir_blue1"
+            },
+            {
+                "color": "藍色",
+                "price": "24900",
+                "storage": "64GB",
+                "capacity": "Wi-Fi+行動網路",
+                "image": "ipadAir_blue1"
+            },
+            {
+                "color": "藍色",
+                "price": "24900",
+                "storage": "256GB",
+                "capacity": "Wi-Fi",
+                "image": "ipadAir_blue1"
+            },
+            {
+                "color": "藍色",
+                "price": "29900",
+                "storage": "256GB",
+                "capacity": "Wi-Fi+行動網路",
+                "image": "ipadAir_blue1"
+            }
         ]
     },
     {
-        "color":"pink",
-        "data":[
-            {"color": "粉紅色", "price": "19900", "storage": "64GB", "capacity": "Wi-Fi", "image": "ipadAir_pink1" },
-            {"color": "粉紅色", "price": "24900", "storage": "64GB", "capacity": "Wi-Fi+行動網路", "image": "ipadAir_pink1" },
-            {"color": "粉紅色", "price": "24900", "storage": "256GB", "capacity": "Wi-Fi", "image": "ipadAir_pink1" },
-            {"color": "粉紅色", "price": "29900", "storage": "256GB", "capacity": "Wi-Fi+行動網路", "image": "ipadAir_pink1" },
+        "color": "pink",
+        "data": [
+            {
+                "color": "粉紅色",
+                "price": "19900",
+                "storage": "64GB",
+                "capacity": "Wi-Fi",
+                "image": "ipadAir_pink1"
+            },
+            {
+                "color": "粉紅色",
+                "price": "24900",
+                "storage": "64GB",
+                "capacity": "Wi-Fi+行動網路",
+                "image": "ipadAir_pink1"
+            },
+            {
+                "color": "粉紅色",
+                "price": "24900",
+                "storage": "256GB",
+                "capacity": "Wi-Fi",
+                "image": "ipadAir_pink1"
+            },
+            {
+                "color": "粉紅色",
+                "price": "29900",
+                "storage": "256GB",
+                "capacity": "Wi-Fi+行動網路",
+                "image": "ipadAir_pink1"
+            }
         ]
     },
     {
-        "color":"purple",
-        "data":[
-            {"color": "紫色", "price": "19900", "storage": "64GB", "capacity": "Wi-Fi", "image": "ipadAir_purple1" },
-            {"color": "紫色", "price": "24900", "storage": "64GB", "capacity": "Wi-Fi+行動網路", "image": "ipadAir_purple1" },
-            {"color": "紫色", "price": "24900", "storage": "256GB", "capacity": "Wi-Fi", "image": "ipadAir_purple1" },
-            {"color": "紫色", "price": "29900", "storage": "256GB", "capacity": "Wi-Fi+行動網路", "image": "ipadAir_purple1" },
+        "color": "purple",
+        "data": [
+            {
+                "color": "紫色",
+                "price": "19900",
+                "storage": "64GB",
+                "capacity": "Wi-Fi",
+                "image": "ipadAir_purple1"
+            },
+            {
+                "color": "紫色",
+                "price": "24900",
+                "storage": "64GB",
+                "capacity": "Wi-Fi+行動網路",
+                "image": "ipadAir_purple1"
+            },
+            {
+                "color": "紫色",
+                "price": "24900",
+                "storage": "256GB",
+                "capacity": "Wi-Fi",
+                "image": "ipadAir_purple1"
+            },
+            {
+                "color": "紫色",
+                "price": "29900",
+                "storage": "256GB",
+                "capacity": "Wi-Fi+行動網路",
+                "image": "ipadAir_purple1"
+            }
         ]
     },
     {
-        "color":"startlight",
-        "data":[
-            {"color": "星光色", "price": "19900", "storage": "64GB", "capacity": "Wi-Fi", "image": "ipadAir_starlight1" },
-            {"color": "星光色", "price": "24900", "storage": "64GB", "capacity": "Wi-Fi+行動網路", "image": "ipadAir_starlight1" },
-            {"color": "星光色", "price": "24900", "storage": "256GB", "capacity": "Wi-Fi", "image": "ipadAir_starlight1" },
-            {"color": "星光色", "price": "29900", "storage": "256GB", "capacity": "Wi-Fi+行動網路", "image": "ipadAir_starlight1" },
+        "color": "startlight",
+        "data": [
+            {
+                "color": "星光色",
+                "price": "19900",
+                "storage": "64GB",
+                "capacity": "Wi-Fi",
+                "image": "ipadAir_starlight1"
+            },
+            {
+                "color": "星光色",
+                "price": "24900",
+                "storage": "64GB",
+                "capacity": "Wi-Fi+行動網路",
+                "image": "ipadAir_starlight1"
+            },
+            {
+                "color": "星光色",
+                "price": "24900",
+                "storage": "256GB",
+                "capacity": "Wi-Fi",
+                "image": "ipadAir_starlight1"
+            },
+            {
+                "color": "星光色",
+                "price": "29900",
+                "storage": "256GB",
+                "capacity": "Wi-Fi+行動網路",
+                "image": "ipadAir_starlight1"
+            }
         ]
     }
 ]
